5978527fc3288e810f5b6c397b8e62bbb7cb7b98
Nicole Salazar <nicolesalazar@nicoles-macbook.local>
2024-10-14 00:34:00 -0400

    added homepage

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,14 @@
+class ProjectsController < ApplicationController
+
+    def index
+        #this is the home page
+    end
+
+    def about
+    end
+
+
+
+
+        
+end
---
+++ app/helpers/projects_helper.rb
@@ -0,0 +1,8 @@
+module ProjectsHelper
+end
+
+def inline_svg(path)
+    File.open("app/assets/images/#{path}", "rb") do |file|
+    raw file.read
+  end
+end
