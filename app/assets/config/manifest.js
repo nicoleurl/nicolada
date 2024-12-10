a4ca7cbea8e9f812535bfaca6e2f8d0a5f3a4826
Nicole Salazar <nicolesalazar@nicoles-macbook.local>
2024-12-10 17:25:21 -0400

    image replacement

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,4 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link jquery.js
 //= link jquery_ujs.js
+//= link swiper.js
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-//= require jquery
-//= require jquery_ujs
 import "./add_jquery"
 import "@hotwired/turbo-rails"
 import "controllers"
