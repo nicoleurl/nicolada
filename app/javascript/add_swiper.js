794ec84b56fc8c34dbd3387f88c54bb6e0ba07c8
Nicole Salazar <nicolesalazar@macbookpro.mynetworksettings.com>
2025-11-02 10:36:46 -0400

    Island Finance Case Study set up, long version

--- app/javascript/add_swiper.js
+++ app/javascript/add_swiper.js
@@ -1,13 +1,38 @@
 import "swiper";
 
-
-$(document).ready(function () {
+function initSwiper() {
     // Initialize Swiper
-    const swiper = new Swiper('.swiper', {
+    const swiper = new Swiper('.personasSwiper', {
+        modules: [Navigation],
         slidesPerView: '1.5',
         centeredSlides: true,
-        spaceBetween: 30,
-    });
+        spaceBetween: 0,
+        allowTouchMove: true,
+        loop: true,
+        freeMode: false,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        cssMode: true,
+        observer: true,
+        observeParents: true,
+        breakpoints: {
+          768: {
+            slidesPerView: 1.2,
+          },
+          500: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+          }
+        },
+      });
 
-    console.log('Swiper initialized:', swiper);
-});
+    document.querySelector('.swiper-button-next').addEventListener('click', () => {
+        swiper.slideNext();
+    });
+    document.querySelector('.swiper-button-prev').addEventListener('click', () => {
+        swiper.slidePrev();
+      });
+    }
+    window.onload = initSwiper;
